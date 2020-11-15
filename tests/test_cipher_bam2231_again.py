23262f2ba108b2296b824df23eac0b6048ba3538
Bryn A. McCarthy <51329948+bam2231@users.noreply.github.com>
2020-11-15 12:33:56 -0500

    Update test_cipher_bam2231_again.py

--- tests/test_cipher_bam2231_again.py
+++ tests/test_cipher_bam2231_again.py
@@ -3,3 +3,9 @@ from cipher_bam2231_again import cipher_bam2231_again
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cipher_1():
+    expected = 'uftujoh'
+    sample_word = 'testing'
+    actual = cipher(sample_word, shift = 1)
+    assert expected == actual
