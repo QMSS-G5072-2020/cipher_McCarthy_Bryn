5825139c9e3f594d5b7d14db874131ce35151a24
Bryn A. McCarthy <51329948+bam2231@users.noreply.github.com>
2020-11-15 12:36:45 -0500

    Update test_cipher_bam2231_again.py

--- tests/test_cipher_bam2231_again.py
+++ tests/test_cipher_bam2231_again.py
@@ -4,6 +4,19 @@ from cipher_bam2231_again import cipher_bam2231_again
 def test_version():
     assert __version__ == '0.1.0'
 
+def cipher(text, shift, encrypt=True):
+    alphabet = 'abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+
 def test_cipher_1():
     expected = 'uftujoh'
     sample_word = 'testing'
